46b40724e5dc023a6224ef257b2aa653d8f0faaf
rendrikson <reigo.hendrikson@gmail.com>
2016-01-15 13:36:58 +0200

    add data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Baby names.
+#'
+#' Full baby name data provided by the SSA. This includes all names with at
+#' least 5 uses.
+#'
+#' @format A data frame with five variables: \code{year}, \code{sex},
+#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
+#'   of applicants in that year).
+"mmspaces_3d"
